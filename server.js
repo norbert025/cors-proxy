a20907bbbb3fe9c282e60530c8f18493da049c31
Norberto Gonzalez <36218596+norbert025@users.noreply.github.com>
2022-02-13 02:55:22 -0500

    update-3

--- server.js
+++ server.js
@@ -15,16 +15,9 @@ app.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
     res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
 
-    if (req.method === 'OPTIONS') {
-        // CORS Preflight
-        res.send();
-    } else {
-        var targetURL = req.header('https://slack.com');
-        if (!targetURL) {
-            res.send(500, { error: 'There is no Target-Endpoint header in the request' });
-            return;
-        }
-        request({ url: targetURL + req.url, method: req.method, json: req.body, headers: {'Authorization': req.header('Authorization')} },
+   
+        request({ url: "https://slack.com/api/conversations.history?channel=C02735NBND7", method: "GET", headers: {
+    Authorization: `Bearer xoxp-2234729482980-2221777343974-3057712731797-645a11818690c1456f12fcbddfc2a9f9` },
             function (error, response, body) {
                 if (error) {
                     console.error('error: ' + response.statusCode)
